706ea1f349463b429c5a4677dfd4c5e706eda6ca
LEO VITOR DE CAMPOS <180976@facens.br>
2020-02-19 10:52:08 -0300

    Server and Client

---
+++ Client/Client/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Client
+{
+    class Program
+    {
+        public static BinaryWriter binaryWriter;
+        public static BinaryReader binaryReader;
+        public static NetworkStream NetworkStream;
+
+        [DllImport("Kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+        [DllImport("User32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int cmdShow);
+
+        static void Main(string[] args)
+        {
+            IntPtr hWnd = GetConsoleWindow();
+            ShowWindow(hWnd, 0);
+            Thread thread = new Thread(new ThreadStart(RunClient));
+            thread.Start();
+        }
+
+        private static void RunClient()
+        {
+            TcpClient client;
+
+            try
+            {
+                client = new TcpClient();
+                client.Connect("localhost", 7777);
+
+                Console.WriteLine("[SERVIDOR]: Conexão estabelecida."); ;
+               
+                NetworkStream = client.GetStream();
+                binaryWriter = new BinaryWriter(NetworkStream);
+                binaryReader = new BinaryReader(NetworkStream);
+
+                var msg = "";
+                do
+                {
+                    msg = Console.ReadLine();
+                    binaryWriter.Write(msg);
+                    //IntPtr hWnd = GetConsoleWindow();
+                    //ShowWindow(hWnd, 1);
+                } while (msg.ToLower() != "fim");
+                //Console.WriteLine(msg);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ Servidor/Servidor/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+
+namespace Servidor
+{
+    class Program
+    {
+        public static Socket connection;
+        public static BinaryWriter binaryWriter;
+        public static BinaryReader binaryReader;
+        public static NetworkStream networkStream;
+
+        static void Main(string[] args)
+        {
+            Thread thread = new Thread(new ThreadStart(RunServer));
+            thread.Start();
+        }
+
+        private static void RunServer()
+        {
+            TcpListener tcpListener;
+
+            try
+            {
+                tcpListener = new TcpListener(7777);
+                tcpListener.Start();
+
+                while (true)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("[SERVIDOR]: Aguardando conexão...");
+                    connection = tcpListener.AcceptSocket();
+                    Console.WriteLine("[SERVIDOR]: Conexão estabelecida. Remote end point: " + connection.RemoteEndPoint); ;
+                    networkStream = new NetworkStream(connection);
+                    binaryWriter = new BinaryWriter(networkStream);
+                    binaryReader = new BinaryReader(networkStream);
+
+                    Random rnd = new Random(DateTime.Now.Millisecond);
+
+                    ConsoleColor connectionColor = (ConsoleColor) rnd.Next(0, 10);
+
+                    string msg;
+
+                    Console.ForegroundColor = connectionColor;
+
+                    do
+                    {
+                        msg = binaryReader.ReadString();
+                        Console.WriteLine(msg, Console.ForegroundColor);
+                    } while (msg.ToLower() != "fim");
+
+                    binaryWriter.Close();
+                    binaryReader.Close();
+                    networkStream.Close();
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
